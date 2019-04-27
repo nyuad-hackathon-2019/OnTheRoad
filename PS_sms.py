cf6d496e3142afa300da09f4f7d0e0ed41b8aed0
masa <masamh@outlook.com>
2019-04-27 11:48:33 +0400

    added setupDB.py and PS_sms.p

---
+++ PS_sms.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+from twilio.rest import Client
+from twilio.twiml.voice_response import VoiceResponse
+from flask import Flask, Response, send_from_directory
+from flask import request
+
+app = Flask(__name__)
+
+ACCOUNT_SID = 'AC94a1765a13973ddfa4420c0d35d6ab4e'
+AUTH_TOKEN = 'e67261b76d3969e46a209a589fab2b69'
+PHONE = '+16266002278'
+PHONE2 = '+971505250332'
+
+@app.route("/", methods=["GET"])
+def hello():
+  return send_from_directory("/", "index.html")
+
+@app.route("/send")
+def send_sms():
+  global client
+  global STATE
+  print 'Sending a message...'
+  new_message = client.messages.create(to=PHONE2, from_='+16266002278', body=WARNING_TITLE)
+  STATE = "WARNING"
+  return "Thank you!"
+
+@app.route("/receive_sms")
+def receive_sms():
+  global STATE
+
+  if STATE == "WARNING":
+    new_message = client.messages.create(to=PHONE2, from_='+16266002278', body=WARNING_DETAILS)
+    STATE = "DETAILS"
+  elif STATE == "DETAILS":
+    #new_message_1 = client.messages.create(to=PHONE2, from_='+16266002278', body=JOB_CONFIRMATION)
+    new_message_2 = client.messages.create(to=PHONE2, from_='+16266002278', body=RECEIVE_SMS)
+    STATE = "OK"
+  else:
+    new_message_1 = client.messages.create(to=PHONE, from_='+16266002278', body='لقد تم التحقق من العملية  بنجاح!')
+    STATE = "WARNING"
+
+  return "OK!"
+
+  
+
+if __name__ == "__main__":
+  app.run()
---
+++ setupDB.py
@@ -0,0 +1,65 @@
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+# create category table
+class SMS(Base):
+    __tablename__ = 'sms'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(400), nullable=False)
+    locationID = Column(String(400), nullable=False)
+    level = Column(String(400), nullable=False)
+    timestamp = Column(String(400), nullable=False)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'title': self.title,
+            'id': self.id,
+            'locationID': self.locationID,
+            'level': self.level,
+            'timestamp': self.timestamp,
+
+        }
+
+
+# create uset table
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    phone = Column(String(250, nullable=False))
+
+
+# create item table
+class Location(Base):
+    __tablename__ = 'location'
+
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    Long = Column(String(400))
+    Lat = Column(String(400))
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'Longitude': self.long,
+            'Latitude': self.Lat,
+            'id': self.id,
+        }
+
+
+engine = create_engine('sqlite:///catalog.db')
+
+
+Base.metadata.create_all(engine)
